4984018479acf69b6314c63c6dffe55abca62b03
Rubén <ruben.vanyo@gmail.com>
2012-11-23 09:35:28 +0100

    probando PCategoria

--- PCategoria/PCategoria/MainWindow.cs
+++ PCategoria/PCategoria/MainWindow.cs
@@ -1,13 +1,45 @@
-using System;
 using Gtk;
+using Npgsql;
+using System;
+using System.Data;
+
 
 public partial class MainWindow: Gtk.Window
 {	
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		fillComboBox();
 	}
+	private void fillComboBox(){
+		CellRenderer cellRenderer = new CellRendererText();
+		comboBox.PackStart(cellRenderer,false); //expand false
+		comboBox.AddAttribute(cellRenderer,"text",1);
+		
+		ListStore listStore = new ListStore(typeof(string),typeof(string));
+		comboBox.Model=listStore;
+		
+		
+		string connectionString = "Server=localhost; Database=prueba; User ID=dbprueba; Password=sistemas";
+		IDbConnection dbConnection = new NpgsqlConnection(connectionString);
+		
+		dbConnection.Open ();
+		
+		
+		IDbCommand dbCommand= dbConnection.CreateCommand();
+		dbCommand.CommandText="select id,nombre from categoria";
+		
+		IDataReader dataReader = dbCommand.ExecuteReader();
 	
+		while(dataReader.Read ())
+		listStore.AppendValues(dataReader["id"].ToString(),dataReader["nombre"].ToString());
+		
+		dataReader.Close();
+		
+		dbConnection.Close();
+	
+	
+	}
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
 		Application.Quit ();
