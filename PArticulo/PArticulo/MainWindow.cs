7c300c438ec73ad40dbc2bebc4e9c2398fce8a0a
Rubén <ruben.vanyo@gmail.com>
2012-12-12 09:04:16 +0100

    añadido PAdSerpis en PArticulo

--- PArticulo/PArticulo/MainWindow.cs
+++ PArticulo/PArticulo/MainWindow.cs
@@ -1,21 +1,22 @@
 using Gtk;
 using Npgsql;
+using PArticulo;
 using Serpis.Ad;
 using System;
 using System.Collections.Generic;
 using System.Data;
 
-using PArticulo;
 
 public partial class MainWindow: Gtk.Window
-{	
+{
 	private IDbConnection dbConnection;
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
 		
-		string connectionString = "Server=localhost;Database=prueba;User Id=dbprueba;Password=sistemas";
-		dbConnection = new NpgsqlConnection(connectionString);
+		string connectionString = "Server=localhost;Database=dbprueba;User Id=dbprueba;Password=sistemas";
+		ApplicationContext.Instance.DbConnection = new NpgsqlConnection(connectionString);
+		dbConnection = ApplicationContext.Instance.DbConnection;
 		dbConnection.Open ();
 		
 		IDbCommand dbCommand = dbConnection.CreateCommand ();
@@ -52,52 +53,8 @@ public partial class MainWindow: Gtk.Window
 	protected void OnEditActionActivated (object sender, System.EventArgs e)
 	{
 		long id = getSelectedId();
-		
-		Console.WriteLine ("id={0}", id);
-		
-		IDbCommand dbCommand = dbConnection.CreateCommand();
-		//dbCommand.CommandText = "select * from articulo where id="+id;
-		dbCommand.CommandText = string.Format ("select * from articulo where id={0}", id);
-//		dbCommand.CommandText = "select * from articulo where id=:id";
-//		IDbDataParameter dbDataParameter = dbCommand.CreateParameter();
-//		dbCommand.Parameters.Add (dbDataParameter);
-//		dbDataParameter.ParameterName = "id";
-//		dbDataParameter.Value = id;
-		
-		IDataReader dataReader = dbCommand.ExecuteReader ();
-		dataReader.Read ();
-		
-		
-		ArticuloView articuloView = new ArticuloView();
-		articuloView.Nombre = (string)dataReader["nombre"];
-		articuloView.Precio = (decimal)dataReader["precio"];
-		
+		ArticuloView articuloView = new ArticuloView( id );
 		articuloView.Show ();
-		
-		dataReader.Close ();
-
-		articuloView.SaveAction.Activated += delegate {
-			Console.WriteLine("articuloView.SaveAction.Activated");
-			
-			IDbCommand dbUpdateCommand = dbConnection.CreateCommand ();
-			dbUpdateCommand.CommandText = "update articulo set nombre=:nombre, precio=:precio where id=:id";
-		
-			
-			AddParameter(dbUpdateCommand,"nombre",articuloView.Nombre);
-			AddParameter(dbUpdateCommand,"precio",articuloView.Precio);
-			AddParameter(dbUpdateCommand,"id",id);
-
-
-//			Si usamos sustitución de cadenas tendremos problemas con:
-//			los "'" en los string, las "," en los decimal y el formato de las fechas
-//			dbUpdateCommand.CommandText = 
-//				String.Format ("update articulo set nombre='{0}', precio={1} where id={2}", 
-//				               articuloView.Nombre, articuloView.Precio, id);
-
-			dbUpdateCommand.ExecuteNonQuery ();
-			
-			articuloView.Destroy ();
-		};
 	}
 	
 	private long getSelectedId() {
@@ -107,12 +64,4 @@ public partial class MainWindow: Gtk.Window
 		ListStore listStore = (ListStore)treeView.Model;
 		return long.Parse (listStore.GetValue (treeIter, 0).ToString ()); 
 	}
-	
-	public static void AddParameter(IDbCommand dbCommand,string name, object value){
-	IDbDataParameter dbDataParameter=dbCommand.CreateParameter();
-		dbDataParameter.ParameterName=name;
-		dbDataParameter.Value=value;
-		dbCommand.Parameters.Add (dbDataParameter);
-		}
-	
 }
