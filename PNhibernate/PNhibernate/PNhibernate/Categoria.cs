2cb6d21f4c1aa5f852a190ee2ba6f125b2740450
Rubén <ruben.vanyo@gmail.com>
2013-02-01 10:37:01 +0100

    empezando con Nhibernate

---
+++ PNhibernate/PNhibernate/PNhibernate/Categoria.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace serpis.Ad
+{
+	public class Categoria
+	{
+		public virtual long Id {get;set;}
+		public virtual string Nombre{get;set;}
+		
+	}
+}
+
--- PNhibernate/PNhibernate/PNhibernate/MainWindow.cs
+++ PNhibernate/PNhibernate/PNhibernate/MainWindow.cs
@@ -1,5 +1,8 @@
 using System;
 using Gtk;
+using NHibernate.Cfg;
+using serpis.Ad;
+using NHibernate.Tool.hbm2ddl;
 using NHibernate;
 
 public partial class MainWindow: Gtk.Window
@@ -7,6 +10,29 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		
+		Configuration configuration = new Configuration();
+		configuration.Configure();
+		configuration.AddAssembly(typeof(Categoria).Assembly);
+		new SchemaExport(configuration).Execute(true,false,false);
+		
+		ISessionFactory sessionFactory=configuration.BuildSessionFactory();
+		
+		ISession session = sessionFactory.OpenSession();
+		
+		Categoria categoria =(Categoria) session.Load(typeof(Categoria),2L);
+		
+        Console.WriteLine("Categoria ID={0} Nombre={1}",categoria.Id,categoria.Nombre);
+
+		categoria.Nombre = DateTime.Now.ToString();
+		 session.SaveOrUpdate(categoria);
+		
+		 
+		
+		session.Close();
+		sessionFactory.Close();
+		
+		
 	}
 	
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
